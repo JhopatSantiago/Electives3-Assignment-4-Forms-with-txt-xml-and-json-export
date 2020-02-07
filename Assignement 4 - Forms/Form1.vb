6bcf4c970dbf748ff48bcf3cba4a8b71b4280698
JhopatSantiago <46297312+JhopatSantiago@users.noreply.github.com>
2020-02-07 23:04:31 +0800

    Export to JSON Function

--- Assignement 4 - Forms/Form1.vb
+++ Assignement 4 - Forms/Form1.vb
@@ -60,4 +60,19 @@ Public Class Form1
             MsgBox("Please input necessary data in the field", MsgBoxStyle.Critical, "Input data")
         End If
     End Sub
+
+    Private Sub json_export_Click(sender As Object, e As EventArgs) Handles json_export.Click
+        Dim sw1 As StreamWriter
+        If fullname_text.Text <> "" Or age_text.Text <> "" Or address_text.Text <> "" Then
+            sw1 = File.AppendText("D:\Documents\School Stuff\Fifth Year\2nd Sem\Electives 3 - Cont\Assignement 4 - Forms\Assignment-4---Forms-with-txt-xml-and-json-export\json file\information.json")
+            sw1.WriteLine("-----------------------------------------")
+            sw1.WriteLine("Name: " & fullname_text.Text)
+            sw1.WriteLine("Age: " & age_text.Text)
+            sw1.WriteLine("Address: " & address_text.Text)
+            MsgBox("JSON file has been successfully created!", MsgBoxStyle.Information, "Saved")
+            sw1.Close()
+        Else
+            MsgBox("Please input necessary data in the field", MsgBoxStyle.Critical, "Input data")
+        End If
+    End Sub
 End Class
