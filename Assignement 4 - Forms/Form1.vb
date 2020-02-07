ee5f1a513eba7f892df67a72f9089e1517d2314b
JhopatSantiago <46297312+JhopatSantiago@users.noreply.github.com>
2020-02-07 21:29:35 +0800

    Code for exporting text file

--- Assignement 4 - Forms/Form1.vb
+++ Assignement 4 - Forms/Form1.vb
@@ -1,3 +1,17 @@
-﻿Public Class Form1
-
+﻿Imports System.IO
+Public Class Form1
+    Private Sub txt_export_Click(sender As Object, e As EventArgs) Handles txt_export.Click
+        Dim sw1 As StreamWriter
+        If fullname_text.Text <> "" Or age_text.Text <> "" Or address_text.Text <> "" Then
+            sw1 = File.AppendText("D:\Documents\School Stuff\Fifth Year\2nd Sem\Electives 3 - Cont\Assignement 4 - Forms\Assignment-4---Forms-with-txt-xml-and-json-export\txt file\information.txt")
+            sw1.WriteLine("-----------------------------------------")
+            sw1.WriteLine("Name: " & fullname_text.Text)
+            sw1.WriteLine("Age: " & age_text.Text)
+            sw1.WriteLine("Address: " & address_text.Text)
+            MsgBox("Text file has been successfully created!", MsgBoxStyle.Information, "Saved")
+            sw1.Close()
+        Else
+            MsgBox("Please input necessary data in the field", MsgBoxStyle.Critical, "Input data")
+        End If
+    End Sub
 End Class
